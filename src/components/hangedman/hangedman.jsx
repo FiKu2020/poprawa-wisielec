a51c7d61e0e4b4d7eaaa52ec45fa0deb543736fe
filip k <filip2006kujawa@gmail.com>
2024-06-13 21:21:05 +0200

    z poprawkami powinno byc ok?

--- src/components/hangedman/hangedman.jsx
+++ src/components/hangedman/hangedman.jsx
@@ -1,22 +1,15 @@
-import React, { useEffect, useState } from "react";
-import PropTypes from "prop-types";
+import React from "react";
+import { Link } from "react-router-dom";
 import Word from "../word/Word";
 import Keyboard from "../keyboard/Keyboard";
-const Hangman = ({ wordsList }) => {
-  const {
-    word,
-    setWord,
-    guessedLetters,
-    setGuessedLetters,
-    attemptsLeft,
-    setAttemptsLeft,
-  } = useHangman();
-
+import { useHangmanContext } from "../../assets/context/hangmanContext";
+const Hangman = () => {
+  const { word, guessedLetters } = useHangmanContext();
   return (
     <div className="hangman-game">
       <h1>Wisielec</h1>
       <Word />
-      <Keyboard/>
+      <Keyboard />
     </div>
   );
 };
